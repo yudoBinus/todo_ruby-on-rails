f65b9ad8326c729152a66a806c6cfa6b802e30e1
Yudo Kartiko <yudobinus@gmail.com>
2021-06-23 11:12:55 +0700

    Add notice flash

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,12 +8,14 @@ class TodosController < ApplicationController
     @todo = Todo.find_by(id: params[:id])
     @todo.destroy
     redirect_to('/todos/index')
+    flash[:notice] = "Successfully delete new todo"
   end
 
   def new
     @todo = Todo.new(todo: params[:todo], status: FALSE)
 
     if @todo.save
+      flash[:notice] = "Successfully create new todo"
       redirect_to('/todos/index')
     else
       @todos = Todo.all
@@ -32,6 +34,7 @@ class TodosController < ApplicationController
     
     if @todo.save
       redirect_to('/todos/index')
+      flash[:notice] = "Successfully update new todo"
     else
       render("todos/update_form")
     end
